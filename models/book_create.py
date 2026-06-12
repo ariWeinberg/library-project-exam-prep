b14782c3f86b2acf5bbce895257ed4918b0f7224
Ari Weinberg <ari@ari.com>
2026-06-12 19:16:03 +0300

    \feat: implement BookCreate model.

---
+++ models/book_create.py
@@ -0,0 +1,24 @@
+from turtle import mode
+
+from pydantic import BaseModel, Field, model_validator
+from typing import Literal
+
+
+class BookCreate(BaseModel):
+    title: str = Field(max_length=50)
+    author: str = Field(max_length=50)
+    genere: str = Literal['Fiction', 'Non-Fiction', 'Science', 'History', 'Other']
+    is_avilable: bool = Field(default=True)
+    borrowed_by_member_id: int | None = Field(gt=0, default=None)
+
+    @model_validator(mode='after')
+    def validate_avilability(self):
+        """Validate that avilability and borrowing rules are kept."""
+        if (self.is_avilable == True) and (self.borrowed_by_member_id is not None):
+            message = f"borrowed_by_member_id can not be set when is_avilable is True."
+            raise ValueError (message)
+
+        if (self.is_avilable == False) and (self.borrowed_by_member_id is None):
+            message = f"borrowed_by_member_id must be set when is_avilable is not."
+            raise ValueError (message)
+
