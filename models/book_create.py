2fcf7a52937e0763d56adb66a55cf94de299b37e
Ari Weinberg <ari@ari.com>
2026-06-12 19:19:59 +0300

    \fix: add aliases and change names inbook models BookCreate model.

--- models/book_create.py
+++ models/book_create.py
@@ -5,11 +5,11 @@ from typing import Literal
 
 
 class BookCreate(BaseModel):
-    title: str = Field(max_length=50)
-    author: str = Field(max_length=50)
-    genere: str = Literal['Fiction', 'Non-Fiction', 'Science', 'History', 'Other']
-    is_avilable: bool = Field(default=True)
-    borrowed_by_member_id: int | None = Field(gt=0, default=None)
+    book_title: str = Field(max_length=50, alias="title")
+    book_author: str = Field(max_length=50, alias="author")
+    book_genere: Literal['Fiction', 'Non-Fiction', 'Science', 'History', 'Other'] = Field(alias="genere")
+    book_is_avilable: bool = Field(default=True, alias="is_avilable")
+    book_borrowed_by_member_id: int | None = Field(gt=0, default=None, alias="borrowed_by_member_id")
 
     @model_validator(mode='after')
     def validate_avilability(self):
