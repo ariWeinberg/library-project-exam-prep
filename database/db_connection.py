90357cfa17436f159ce178ce26aae4ce0ee5ca9c
Ari Weinberg <ari@ari.com>
2026-06-13 23:47:26 +0300

    \feat: implement db_connection.py .

---
+++ database/db_connection.py
@@ -0,0 +1,64 @@
+import os
+from dotenv import load_dotenv
+from mysql.connector import connect
+from mysql.connector.abstracts import MySQLCursorAbstract, MySQLConnectionAbstract
+
+
+load_dotenv()
+
+STMT_CREATE_TABLE_BOOKS = (
+    "CREATE TABLE IF NOT EXISTS books ("
+    "`id` INT PRIMARY KEY AUTO_IMCREMENT NOT NULL,"
+    "`title` VARCHAR(50) NOT NULL,"
+    "`author` VARCHAR(50) NOT NULL,"
+    "`genere` ENUM('Fiction', 'Non-Fiction', 'Science', 'History', 'Other'),"
+    "`is_avilable` BOOLEAN DEFAULT TRUE NOT NULL,"
+    "`borrowed_by_member_id` INT DEFAULT NULL,"
+    ");"
+)
+STMT_CREATE_TABLE_MEMBERS = (
+    "CREATE TABLE IF NOT EXISTS members ("
+    "`id` INT PRIMARY KEY AUTO_IMCREMENT NOT NULL,"
+    "`name` VARCHAR(50) NOT NULL,"
+    "`email` VARCHAR(50) UNIQUE NOT NULL,"
+    "`is_active` BOOLEAN DEFAULT TRUE NOT NULL,"
+    "`total_borrows` INT DEFAULT 0 CHECK(total_borrows >= 0),"
+    ");"
+)
+
+db_connection_settings = {
+    'host': os.environ.get("DB_HOST", "localhost"),
+    'port': os.environ.get("DB_PORT", "3306"),
+    'user': os.environ.get("DB_USER", "root"),
+    'password': os.environ.get("DB_PASSWORD", "root"),
+    'database': os.environ.get("DB_NAME", "library_db")
+}
+
+
+def get_connection() -> MySQLConnectionAbstract:
+    return connect(**db_connection_settings)
+
+
+def create_tables():
+    conn: MySQLConnectionAbstract = get_connection()
+    try:
+        cur: MySQLCursorAbstract = conn.cursor()
+
+        _create_table_books(cur=cur)
+        _create_table_members(cur=cur)
+
+        cur.close()
+        conn.commit()
+    finally:
+        conn.close()
+
+def _create_table_books(cur: MySQLCursorAbstract):
+    cur.execute(STMT_CREATE_TABLE_BOOKS)
+
+def _create_table_members(cur: MySQLCursorAbstract):
+    cur.execute(STMT_CREATE_TABLE_MEMBERS)
+
+
+
+get_connection()
+
