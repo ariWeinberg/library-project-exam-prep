04279cd443a927badb409aba6ba158a7ef1d7fc6
Ari Weinberg <ari@ari.com>
2026-06-14 03:01:20 +0300

    \re: add a response model for book creation route.

--- routes/book_routes.py
+++ routes/book_routes.py
@@ -1,4 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status, Body
+from models.book_view import BookView
+from models.book_create import BookCreate
+from database.book_db import BookDB
 
 
 router = APIRouter()
+
+
+@router.post("/",
+              status_code=201,
+              response_model=BookView)
+def create_book(book: BookCreate = Body()):
+    """Handles creation of a new book."""
+    book_db=BookDB()
+    try:
+        new_book_id = book_db.create_book(book)
+        new_book = book_db.get_book_by_id(new_book_id)
+
+        return new_book
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail=str(e)
+            )
+
